dfd97f5696351ec7a7afb4e66c51eb6baea4ca7f
Samir-Sadaf25 <samirsadaf.25@gmail.com>
2025-06-21 00:49:44 +0600

    server inital set up and all jobs fetching done

---
+++ index.js
@@ -0,0 +1,95 @@
+require('dotenv').config()
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 3000;
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Job portal server is running');
+})
+app.listen(port, () => {
+    console.log(`job portal is running on port:${port}`)
+})
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vvycnhh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+
+        // Send a ping to confirm a successful connection
+        //await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+        const jobCollection = client.db("CareerCrafters").collection("jobs");
+        // const userCollection = client.db("RecipeBookApp").collection("users")
+
+        app.get('/jobs', async (req, res) => {
+            const result = await jobCollection.find().toArray();
+            res.send(result);
+        })
+
+        app.get('/jobs/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await recipeCollection.findOne(query);
+            res.send(result);
+        })
+        // app.post('/Recipes', async (req, res) => {
+        //     const newRecipe = req.body;
+        //     const result = await recipeCollection.insertOne(newRecipe);
+        //     res.send(result)
+        // })
+
+        // app.get('/jobs/:id', async (req, res) => {
+        //     const id = req.params.id;
+        //     const result = await recipeCollection.find({ userId: userId }).toArray();
+        //     res.send(result);
+        // });
+
+        // app.delete('/Recipes/:id', async (req, res) => {
+        //     const id = req.params.id;
+        //     const result = await recipeCollection.deleteOne({ _id: new ObjectId(id) });
+        //     res.send(result);
+        // });
+
+        // app.put('/Recipes/:id', async (req, res) => {
+        //     const id = req.params.id
+        //     const filter = { _id: new ObjectId(id) }
+        //     const options = { upsert: true };
+        //     const updatedRecipe = req.body;
+        //     const updateDoc = {
+        //         $set: updatedRecipe
+        //     };
+        //     const result = await recipeCollection.updateOne(filter, updateDoc, options)
+        //     res.send(result);
+        // })
+
+
+        // user related API's
+        // app.get('/users', async (req, res) => {
+        //     const result = await userCollection.find().toArray();
+        //     res.send(result);
+        // })
+        // app.post('/users', async (req, res) => {
+        //     const userProfile = req.body;
+        //     const result = await userCollection.insertOne(userProfile);
+        //     res.send(result);
+        // })
+    } finally {
+        // Ensures that the client will close when you finish/error
+
+    }
+}
+run().catch(console.dir);
